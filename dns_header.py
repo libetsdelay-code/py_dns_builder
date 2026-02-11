418b876e993ce38554a6190e0d4325afb380ac87
swank <maximecln01@gmail.com>
2026-02-11 23:00:19 +0100

    Code for encode dns name into valid bytes name readable

--- dns_header.py
+++ dns_header.py
@@ -25,9 +25,36 @@ class DNSQuestion:
     name: bytes
     type_: int
     class_: int
-    
-
 
 1.2
 
+def header_to_bytes(header):
+    fields = dataclasses.astuple(header)
+     # there are 6 `H`s because there are 6 fields
+    return struct.pack("!HHHHHH, *fields")
 
+
+def questions_to_bytes(question):
+    return question.name + struct.pack("!HH, questions.type_, question.class_")
+
+def encode_dns_name(domain_name):
+    encoded = b""
+    for part in domain_name.encode("ascii").split(b"."):
+        print(part)
+        encoded+=  bytes([len(part)]) + part 
+    return encoded + b"\x00"
+
+print(encode_dns_name("google.com"))
+
+1.4 #build the query 
+
+import random
+random.seed(67)
+
+TYPE_A = 1
+CLASS_IN = 1
+
+def build_query(domain_name, record_type):
+    name = encode_dns_name(domain_name)
+    id = random.randint(0, 655535)
+    
